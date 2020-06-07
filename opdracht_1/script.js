b66a86e90aa9df08930645dce83b324e175d2647
Christelva <cvanaanholt@hotmail.com>
2020-06-07 21:27:38 +0200

    Add files via upload

---
+++ opdracht_1/script.js
@@ -0,0 +1,7 @@
+const ikRockArrowFunctions = () => { console.log("Joe, ik rock de arrow functions!")};
+const ikRockArrowFunctions = () =>  console.log("Joe, ik rock de arrow functions!");
+const fivePlusSeven = () => {return 5 + 7};
+() => {return 5 + 7};
+const myFunction = (a,b) => {return a + b};
+const myFunction = a => {return a + 5};
+const createObject = () => {return ({greeting: "hoi"})};
