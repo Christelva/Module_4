621db5596b7678666443c9d7dfea8951c3d191d3
Christelva <cvanaanholt@hotmail.com>
2020-06-11 16:54:32 +0200

    Add files via upload

---
+++ opdracht_2/script.js
@@ -0,0 +1,18 @@
+const array = [
+    { name: "N. Armstrong", profession: "spacecowboy", age: 89 },
+    { name: "H. de Haan", profession: "kippen hypnotiseur", age: 59 },
+    { name: "A. Curry", profession: "kikvorsman", age: 32 },
+    { name: "F. Vonk", profession: "slangenmelker", age: 36 },
+    { name: "B. Bunny", profession: "konijnen uitlaatservice", age: 27 },
+    { name: "Dr.Evil", profession: "digital overlord", age: 56 }
+  ];
+  
+  for (let person of array) {
+    if (person.age > 50){
+        console.log(person.name, "is older than 50");
+      console.log("This is a ",person);
+      console.log("This is ", person.name);
+      console.log(person.name, " was born in ",2020-person.age);
+      console.log(person.name," is a ", person.profession);
+      }
+  }
